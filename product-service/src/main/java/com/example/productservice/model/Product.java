40a959a8dbf94786a6d35a2e154852da121f98b7
Pragathi-V <pragathivadigeri3@gmail.com>
2023-11-22 14:27:33 +0530

    Day 2 commit

--- product-service/src/main/java/com/example/productservice/model/Product.java
+++ product-service/src/main/java/com/example/productservice/model/Product.java
@@ -1,25 +1,24 @@
 package com.example.productservice.model;
 
+import java.math.BigDecimal;
+
+import org.springframework.data.mongodb.core.mapping.Document;
+
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import org.springframework.data.annotation.Id;
-import org.springframework.data.mongodb.core.mapping.Document;
 
-import java.math.BigDecimal;
-
-@Document(value = "product")
-@AllArgsConstructor
+@Document(value="product")
 @NoArgsConstructor
+@AllArgsConstructor
 @Builder
 @Data
-public class Product {
 
-    @Id
-    private String id;
-    private String name;
-    private String description;
-    private BigDecimal price;
-	
+public class Product {
+	private String id;
+	private String name;
+	private String description;
+	private BigDecimal price;
+
 }
